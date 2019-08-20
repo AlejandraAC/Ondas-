bbf112845bbf4831916150101cee1ce2fe420951
AlejandraAC <50935905+AlejandraAC@users.noreply.github.com>
2019-08-20 01:28:02 -0500

    Update wavelet.m

--- wavelet.m
+++ wavelet.m
@@ -1,7 +1,7 @@
 
 function [f,nw2] = wavelet(dt2,freq)
 
-%Esta funcion crea una fuente impulso 
+%Esta funci√≥n crea una fuente impulso que genera una ond√≠cula gaussiana establecida por J. Carcione, (2006)
 
 t0=(6/(5*freq)); %tiempo de retraso o fase
 wb=2*pi*freq; %frecuencia angular central
@@ -17,25 +17,27 @@ for n=1:nw2
     f(n)=exp(-(Dw*Dw*D*D)/4)*cos(wb*D);
 end
 
-%CASO VISCOEL¡STICO
+%CASO VISCOEL√ÅSTICO
     %Se crea un vector con la escala en tiempo
-  %   vect = [0:dt2:(nw2-1)*dt2];
-    % Se grafica la fuente
+ %   vect = [0:dt2:(nw2-1)*dt2];
+    %Se grafica la fuente
   %   plot(vect,f)
-  %   title(['OndÌcula fuente con F=' num2str(freq) '[Hz]'],'Fontsize',19,'FontName','Arial', 'FontWeight', 'bold','FontAngle','italic','HorizontalAlignment','center')
-  %   xlabel('Tiempo [s]','Fontsize',15,'FontWeight','bold' ) %nombre y tamaÒo de ejes
-  %   ylabel('Amplitud','Fontsize',15,'FontWeight', 'bold')
+   %  title(['Ond√≠cula fuente con F = ' num2str(freq) ' [Hz] '],'Fontsize',19,'FontName','Arial', 'FontWeight', 'bold','HorizontalAlignment','center')
+     %Nombre y tama√±o de ejes
+    % xlabel('Tiempo [s]','Fontsize',15,'FontWeight','bold' )
+    % ylabel('Amplitud','Fontsize',15,'FontWeight', 'bold')
 
 
-%CASO ELECTROMAGN…TICO
+%CASO ELECTROMAGN√âTICO
     %Se reescribe la frecuencia en MHz 
     fh=freq*(10^-6);
     %Se crea un vector con la escala en tiempo
     vect = [0:dt2:(nw2-1)*dt2]*(10^9);
-    % Se grafica la fuente
+   % Se grafica la fuente
     plot(vect,f)
-    title(['OndÌcula fuente con F=' num2str(fh) '[MHz]'],'Fontsize',19,'FontName','Arial', 'FontWeight', 'bold','FontAngle','italic','HorizontalAlignment','center')
-    xlabel('Tiempo [ns]','Fontsize',15,'FontWeight','bold' ) %nombre y tamaÒo de ejes
+    title(['Ond√≠cula fuente con F = ' num2str(fh) ' [MHz] '],'Fontsize',19,'FontName','Arial', 'FontWeight', 'bold','HorizontalAlignment','center')
+    %Nombre y tama√±o de ejes
+    xlabel('Tiempo [ns]','Fontsize',15,'FontWeight','bold' ) 
     ylabel('Amplitud','Fontsize',15,'FontWeight', 'bold')
 end
 
